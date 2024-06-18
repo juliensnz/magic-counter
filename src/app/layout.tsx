d4f88031bd283344c8f03f5780f7958af3670e40
Julien Sanchez <juliens@nchez.fr>
2024-06-18 21:09:30 +0200

    simple pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,18 @@ const inter = Inter({subsets: ['latin']});
 export const metadata: Metadata = {
   title: 'Magic counter',
   description: 'Count life points for Magic: The Gathering',
+  viewport: 'width=device-width, initial-scale=1, user-scalable=no',
+  appleWebApp: {
+    capable: true,
+    title: 'agic counter',
+    statusBarStyle: 'default',
+  },
 };
+/*<meta name="viewport" content="user-scalable=no">
 
+<meta name="apple-mobile-web-app-title" content="XXXXXX">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">*/
 export default function RootLayout({
   children,
 }: Readonly<{
